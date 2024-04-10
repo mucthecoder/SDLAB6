8b3ede119db90b887e823e4dbd07daf8394fa577
Mukhathutsheli <charlesndou454@gmail.com>
2024-04-10 23:51:02 +0200

    dddd

--- api/index.js
+++ api/index.js
@@ -1,55 +1,52 @@
-const cars=require('./cars.json');
-let next_ind=0;
-for (let i=0;i<cars.length;i++){
-    let c={
-        make:cars[i].make,
-        model:cars[i].model,
-        year:cars[i].year,
-        price:cars[i].price,
-        id:i
-    };
-    next_ind++;
-    cars[i]=c;
-}
+//create cars api using express
+const express = require('express');
+const app = express();
 
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
 
-    //const name = (req.query.name || (req.body && req.body.name));
-    // const responseMessage = name
-    //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-    
-    if (req.body.key==="get"){
-        context.res.json( {
-            // status: 200, /* Defaults to 200 */
-            body: "responseMessage",
-            text:cars
-        });
-    }
-    else if(req.body.key==="add"){
-        let cari=req.body.one;
-        let c={
-            make:cari.make,
-            model:cari.model,
-            year:cari.year,
-            price:cari.price,
-            id:next_ind
-        };
-        next_ind++;
-        cars.push(c);
-        context.res.json( {
-            // status: 200, /* Defaults to 200 */
-            body: "all good",
-        });
-    }
-    else if(req.body.key==="delete"){
-        const index = cars.findIndex(car => car.id === req.body.one);
-        cars.splice(index, 1);
-        context.res.json( {
-            // status: 200, /* Defaults to 200 */
-            text: cars,
-            what: "responseMessage"
-        });
-    }
-}
+
+app.use(express.json());
+
+const cars = require('./cars.json');
+
+//get all cars
+app.get('/cars', (req, res) => {
+    res.json(cars);
+});
+
+//get car by id
+app.get('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const car = cars.find(car => car.id === id);
+    res.json(car);
+});
+
+//update car
+app.put('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedCar = req.body;
+    const index = cars.findIndex(car => car.id === id);
+    cars[index] = updatedCar;
+    res.json(updatedCar);
+});
+
+//delete car
+app.delete('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const index = cars.findIndex(car => car.id === id);
+    cars.splice(index, 1);
+    res.json({ message: `Car with id ${id} deleted` });
+});
+
+//add car
+app.post('/cars', (req, res) => {
+    console.log(req);
+    const newCar = req.body;
+    console.log(newCar);
+    cars.push(newCar);
+    res.json(newCar);
+});
+
+//start app at localhost:3001
+app.listen(3001, () => {
+    console.log('Server started at http://localhost:3001');
+});
